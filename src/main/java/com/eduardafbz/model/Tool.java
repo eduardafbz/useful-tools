191bdcaeb5630ffcab7ce90874af58bf5b871cb5
eduardafbz <mariaeduardafbraz@outlook.com>
2025-08-01 13:58:01 -0300

    feat: adiciona anotações e configurações para conexão com o banco de dados

--- src/main/java/com/eduardafbz/model/Tool.java
+++ src/main/java/com/eduardafbz/model/Tool.java
@@ -2,10 +2,13 @@ package com.eduardafbz.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,6 +17,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "tools")
 public class Tool {
     
     @Id
@@ -21,6 +25,7 @@ public class Tool {
     @JsonProperty("id")
     private Long id;
 
+    @Column(nullable = false)
     @JsonProperty("name")
     private String name;
 
@@ -30,6 +35,7 @@ public class Tool {
     @JsonProperty("description")
     private String description;
 
+    @ElementCollection //armazena uma lista simples
     @JsonProperty("tags")
     private String tags;
     
