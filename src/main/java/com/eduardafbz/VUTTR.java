657b70017df615786e103c9e4aba707c4156bd1e
eduardafbz <mariaeduardafbraz@outlook.com>
2025-08-11 14:39:24 -0300

    feat: adiciona a classe principal para execução da aplicação

---
+++ src/main/java/com/eduardafbz/VUTTR.java
@@ -0,0 +1,11 @@
+package com.eduardafbz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VUTTR {
+    public static void main(String[] args) {
+        SpringApplication.run(VUTTR.class, args);
+    }
+}
